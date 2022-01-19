f7d7c6f78f9fbd87e829ad55fc914a9f44a8a0bd
Arman173 <armanhaz173@gmail.com>
2022-01-18 20:20:14 -0500

    app cola finished

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,62 @@
+// Referencias al HTMl
+const lblEscritorio = document.querySelector('h1');
+const btnAtender    = document.querySelector('button');
+const lblTicket     = document.querySelector('small');
+const divAlerta     = document.querySelector('.alert');
+const lblPendientes = document.querySelector('#lblPendientes');
 
 
-console.log('Escritorio HTML');
+const searchParams = new URLSearchParams( window.location.search );
+
+if( !searchParams.has('escritorio') ) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerHTML = escritorio;
+
+divAlerta.style.display = 'none';
+
+
+// socket
+const socket = io();
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+    btnAtender.disabled = true;
+});
+
+socket.on('tickets-pendientes', pendientes => {
+    if ( pendientes === 0 ) {
+        divAlerta.style.display = '';
+        lblPendientes.innerHTML = '';
+    } else {
+        divAlerta.style.display = 'none';
+        lblPendientes.innerHTML = pendientes;
+    }
+})
+
+
+btnAtender.addEventListener( 'click', () => {
+
+    socket.emit('atender-ticket', { escritorio }, ( { ok, ticket, msg } ) => {
+        
+        if( !ok ) {
+            lblTicket.innerHTML = 'Nadie.'
+            return divAlerta.style.display = ''
+        };
+
+        lblTicket.innerHTML = `Ticket: ${ ticket.numero }`;
+    });
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+    //     lblNuevoTicket.innerHTML = ticket;
+    // });
+
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,22 +1,52 @@
+const TicketControl = require("../models/ticket-control");
 
-
+const ticketControl = new TicketControl;
 
 const socketController = (socket) => {
+
+    socket.emit( 'ultimo-ticket', ticketControl.ultimo );
+    socket.emit( 'estado-actual', ticketControl.ultimos4 );
+    socket.emit( 'tickets-pendientes', ticketControl.tickets.length );
     
-    console.log('Cliente conectado', socket.id );
-
-    socket.on('disconnect', () => {
-        console.log('Cliente desconectado', socket.id );
-    });
-
-    socket.on('enviar-mensaje', ( payload, callback ) => {
+    socket.on('siguiente-ticket', ( payload, callback ) => {
         
-        const id = 123456789;
-        callback( id );
+        const siguiente = ticketControl.siguiente();
+        callback( siguiente );
 
-        socket.broadcast.emit('enviar-mensaje', payload );
-
+        // TODO: Notificar que hay un nuevo ticket pendiente de asignar
+        socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length );
     })
+
+    socket.on('atender-ticket', ( { escritorio }, callback ) => {
+
+        if ( !escritorio ) {
+            return callback({
+                ok: false,
+                msg: 'El escritorio es obligatorio'
+            });
+        }
+
+        const ticket = ticketControl.atenderTicket( escritorio );
+
+        // TODO: Notificar cambio en los ultimos4
+        socket.broadcast.emit( 'estado-actual', ticketControl.ultimos4 );
+        // TODO: Notificar un ticket menos pendiente de asignar
+        socket.emit( 'tickets-pendientes', ticketControl.tickets.length ); // otra opcion para igual notificar al cliente que emite
+        socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length );
+
+        if ( !ticket ) {
+            callback({
+                ok: false,
+                msg: 'Ya no hay tickets pendientes',
+            });
+        } else {
+            callback({
+                ok: true,
+                ticket,
+                // tickets: ticketControl.tickets.length
+            });
+        }
+    });
 
 }
 
